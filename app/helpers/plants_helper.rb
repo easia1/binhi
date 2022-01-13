dc8f37cba576f48ceef2eaf68095d0b08e83e0eb
Edric Emmanuel Sia <edric.sia@gmail.com>
2022-01-14 03:58:19 +0800

    Added helper for dropdown

--- app/helpers/plants_helper.rb
+++ app/helpers/plants_helper.rb
@@ -1,2 +1,3 @@
 module PlantsHelper
+    
 end
---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,10 @@
+module UsersHelper
+    def occupation_options
+        [
+            ['Student'],
+            ['Educator'],
+            ['Professional'],
+            ['Others']
+        ]
+    end
+end
