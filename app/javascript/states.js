8bc0508291034942cccd99a515d3b709395f2339
JohnCarreos <jrcarreos1@gmail.com>
2022-01-22 13:08:43 +0800

    Add forms for flower color, foliage color and light requirements

--- app/javascript/states.js
+++ app/javascript/states.js
@@ -3,5 +3,12 @@ import 'select2/dist/css/select2.css'
 import 'select2'
 
 window.addEventListener('DOMContentLoaded', () => {
-  $('.js-source-states').select2()
+  $('#plant_light_reqt, #plant_foliage_color, #plant_flower_color').select2({
+    tags: true,
+  })
+  $('#plant_light_reqt').on('select2:opening select2:closing', function( event ) {
+    var $searchfield = $(this).parent().find('.select2-search__field');
+    $searchfield.prop('disabled', true);
+  })
 })
+
